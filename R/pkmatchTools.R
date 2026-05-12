9eea025fb1140e54fbbacea369aaf914567428d7
Daniel Jacob <daniel.jacob@inrae.fr>
2026-05-12 12:24:27 +0200

    1.6.4

--- R/pkmatchTools.R
+++ R/pkmatchTools.R
@@ -444,7 +444,7 @@ clusterMerging <- function(clust1,clust2,dfident1=NULL,dfident2=NULL)
    }
 
    annottab <- clustertab
-   for (i in 1:nrow(cl$dfident))
+   for (i in 1:nrow(dfident))
       annottab[which(annottab[, 2] == dfident[i, 2]), 2] <- dfident[i,1]
 
    list(clusters=clusters, clustertab=clustertab, dfident=dfident, annottab=annottab, params=list (  method='merging' ))
